3264f6bede41a8c1ebfa9d0a13ef319cbc2dd1ac
Jonathan Leonard <johanatan@gmail.com>
2011-09-29 21:23:14 -0700

    Fixed dial operation. Fixed clip function. Added 'pace'parameter.

--- maschine.scm
+++ maschine.scm
@@ -6,10 +6,10 @@
 
 (define dials (make-vector (vector-length samplers)
       (list
-         (list "pitch" "volume" "chop" "attack" "decay" "sustain" "release") ;; parameter
-         (list  7       7        6      6        6       6         6)        ;; channel
-         (list  1       2        3      4        5       6         7)        ;; dial
-         (list  60      100      0      64       64      64        64))))    ;; value
+         (list "pitch" "volume" "pace" "duration" "attack" "decay" "sustain" "release") ;; parameter
+         (list  7       7        7      6          6        6       6         6)        ;; channel
+         (list  11      12       13     14         15       16      17        18)       ;; dial
+         (list  60      100      64     64         64       64      64        64))))    ;; value
 
 (define (set-dial-by-index dial-set i val)
    (set-list-ref (car (cdddr dial-set)) i val))
@@ -30,32 +30,40 @@
 (define-simple-syntax (adjust param value)
    (set! param value))
 
-(define (clip val minimum maximum)
+(define (clip minimum maximum val)
    (max (min val maximum) minimum))
+
+(define (scale minimum maximum val)
+   (truncate (+ minimum (* (/ val 127) maximum))))
 
 (define-simple-syntax (toggle param value)
    (set! param (> value 0)))                   
 
+(define (get-param-val index parameter)
+   (get-dial-val (vector-ref dials index) parameter))
+
 (define (play-sample-vol index volume)
    (play-note (now) (vector-ref samplers index)
-              (get-dial-val (vector-ref dials index) "pitch")
+              (get-param-val index "pitch")
               (clip 0 127 volume)
-              (* 8000 (get-dial-val (vector-ref dials index) "decay"))))
+              (* 8000 (get-param-val index "duration"))))
 
 (define (play-sample index)
-   (play-sample-vol index (get-dial-val (vector-ref dials index) "volume")))
+   (play-sample-vol index (get-param-val index "volume")))
 
 (define cur-pad 0)
 (define (handle-dial channel dial value)
    (cond
-      ((and (>= dial 1) (<= dial 7))
+      ((and (>= dial 11) (<= dial 17))
          (set-dial-by-id (vector-ref dials cur-pad) dial value)
-         (print (vector-ref dials cur-pad))
-         (play-sample cur-pad))))
+         (print (vector-ref dials cur-pad)))))
 
 (define pad-translation (vector 12 13 14 15 8 9 10 11 4 5 6 7 0 1 2 3))
 (define (translate-pad pad)
    (vector-ref pad-translation pad))
+
+(define (get-pace index)
+   (scale 250 7500 (get-param-val cur-pad "pace")))
 
 (define note-repeat #f)
 (define (handle-pad channel pad velocity)
@@ -65,9 +73,9 @@
          (print (vector-ref dials cur-pad))
          (if (> velocity 0)
             (begin
-               (play-sample-vol cur-pad (+ 35 velocity))
+               (play-sample-vol cur-pad (min (+ 35 velocity) (get-param-val cur-pad "volume")))
                (if note-repeat
-                  (callback (+ (now) 5000) 'handle-pad channel pad velocity)))))
+                  (callback (+ (now) (get-pace cur-pad)) 'handle-pad channel pad velocity)))))
       ((= pad 127) (toggle note-repeat velocity))))
 
 (define-simple-syntax (midi-log)
